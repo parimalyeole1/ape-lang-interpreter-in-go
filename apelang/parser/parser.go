d673e8b5b9cc0654c0cdf4b47bc63716e84b551f
Parimal Yeole <parimal.yeole1@gmail.com>
2023-06-22 13:38:43 +0530

    Parser now can report errors

--- apelang/parser/parser.go
+++ apelang/parser/parser.go
@@ -4,12 +4,14 @@ import (
 	"apelang/ast"
 	"apelang/lexer"
 	"apelang/token"
+	"fmt"
 )
 
 type Parser struct {
 	l         *lexer.Lexer
 	curToken  token.Token
 	peekToken token.Token
+	errors    []string
 }
 
 func (p *Parser) nextToken() {
@@ -30,17 +32,30 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next Token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
